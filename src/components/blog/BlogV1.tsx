f271815ca1c569a68baa3d3d9c4078ed2fb49dc8
Rashad Collins <63428010+rcollins22@users.noreply.github.com>
2025-08-10 20:49:41 -0400

    Updated BlogV1.tsx

--- src/components/blog/BlogV1.tsx
+++ src/components/blog/BlogV1.tsx
@@ -10,7 +10,7 @@ const BlogV1 = () => {
                         <div className="col-xl-6 offset-xl-3 col-lg-8 offset-lg-2">
                             <div className="site-heading text-center">
                                 <h4 className="sub-title">Blog Insight</h4>
-                                <h2 className="title">Valuable insights to change your startup idea</h2>
+                                <h2 className="title">My Lastest Thoughts and Deep Dives</h2>
                             </div>
                         </div>
                     </div>
